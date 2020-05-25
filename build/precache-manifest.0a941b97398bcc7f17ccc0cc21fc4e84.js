0fd20e76a62c6ab7ffad0cd8da7f39eb6c0a35bd
amahashabde <36251283+amahashabde@users.noreply.github.com>
2020-05-25 16:41:58 -0700

    Adding files from build that gets uploaded to S3 bucket

---
+++ build/precache-manifest.0a941b97398bcc7f17ccc0cc21fc4e84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c57471794e0bfa840d8325b778a698",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e818787dfff2b111d8e0",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "c4b66cb732e71c34c8e4",
+    "url": "/static/js/2.fc5fd75d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.fc5fd75d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e818787dfff2b111d8e0",
+    "url": "/static/js/main.f0c1273d.chunk.js"
+  },
+  {
+    "revision": "e31a1ace438228e00d0f",
+    "url": "/static/js/runtime-main.5635d6b2.js"
+  }
+]);
